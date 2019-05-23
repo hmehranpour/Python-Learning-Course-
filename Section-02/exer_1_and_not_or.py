f3e1f40e9b2ea7263927b57ef6caad2c74063da0
Hamid <hmehranpour@users.noreply.github.com>
2019-05-23 20:50:23 +0430

    section 2 rev3

--- Section-02/exer_1_and_not_or.py
+++ Section-02/exer_1_and_not_or.py
@@ -1,6 +1,10 @@
-# num_1<-------<num_2 or
+# example :
 num_1 = 10
 num_2 = 20
 num_3 = 30
 num_4 = 40
-# question : input number
+num_5 = 12
+result_1 = (num_1 < num_5 < num_2) or (num_3 < num_5 < num_4)
+print(result_1)
+result_2 = (num_1 < num_5 < num_4) and not (num_2 < num_5 < num_3)
+print(result_2)
